a2976e88d0a86c1cdfa7a7902e9d851c6331f395
balikis21 <kamorudeentemitope170@gmail.com>
2024-08-08 12:02:17 -0700

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,94 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+    1: {
+        "author": "Chinua Achebe",
+        "title": "Things Fall Apart",
+        "reviews": {
+            "critic": "A lack of insight and depth.",
+            "user": "A dull and uninspired narrative."
+        },
+        "ISBN": "978-0-03-097083-8"
+    },
+    2: {
+        "author": "Hans Christian Andersen",
+        "title": "Fairy tales",
+        "reviews": {
+            "critic": "Commonplace and mundane.",
+            "user": "A serious and bleak collection."
+        },
+        "ISBN": "978-1-55-110036-3"
+    },
+    3: {
+        "author": "Dante Alighieri",
+        "title": "The Divine Comedy",
+        "reviews": {
+            "critic": "Simple and straightforward.",
+            "user": "Unremarkable and flat."
+        },
+        "ISBN": "978-0-14-044895-5"
+    },
+    4: {
+        "author": "Evelyn Roberts",
+        "title": "The Epic Of Gilgamesh",
+        "reviews": {
+            "critic": "A tedious and uninspired epic.",
+            "user": "Lacks excitement and interest."
+        },
+        "ISBN": "978-0-14-044100-0"
+    },
+    5: {
+        "author": "Felix Montgomery",
+        "title": "The Book Of Job",
+        "reviews": {
+            "critic": "Clear and unchallenging.",
+            "user": "Prosaic and uneventful."
+        },
+        "ISBN": "978-0-06-621177-3"
+    },
+    6: {
+        "author": "Lydia Fields",
+        "title": "One Thousand and One Nights",
+        "reviews": {
+            "critic": "Unoriginal and repetitive.",
+            "user": "Monotonous and unexciting."
+        },
+        "ISBN": "978-0-19-283447-5"
+    },
+    7: {
+        "author": "Oscar Harrington",
+        "title": "Njál's Saga",
+        "reviews": {
+            "critic": "Boring and flat.",
+            "user": "Dull and lacking in detail."
+        },
+        "ISBN": "978-1-86064-358-8"
+    },
+    8: {
+        "author": "Jane Austen",
+        "title": "Pride and Prejudice",
+        "reviews": {
+            "critic": "Unimaginative and uninspired.",
+            "user": "A bleak and uninvolving read."
+        },
+        "ISBN": "978-0-14-143951-8"
+    },
+    9: {
+        "author": "Honoré de Balzac",
+        "title": "Le Père Goriot",
+        "reviews": {
+            "critic": "Lackluster and superficial.",
+            "user": "A boring and flat narrative."
+        },
+        "ISBN": "978-0-14-044489-2"
+    },
+    10: {
+        "author": "Samuel Beckett",
+        "title": "Molloy, Malone Dies, The Unnamable, the trilogy",
+        "reviews": {
+            "critic": "Vibrant and engaging.",
+            "user": "Stimulating and full of energy."
+        },
+        "ISBN": "978-0-8052-0691-0"
+    }
+};
 
 module.exports=books;
